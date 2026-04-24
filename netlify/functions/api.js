ab559fe3b6c8a789f8f3fda2e95e8db0a7fb0056
hgl <longhg903@gmail.com>
2026-04-24 23:08:09 +0700

    fix cors error

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -35,8 +35,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true                
+  origin: process.env.ALLOWED_ORIGIN || 'http://localhost:5173', 
+  credentials: true                  
 }));
 const router = express.Router()
 
